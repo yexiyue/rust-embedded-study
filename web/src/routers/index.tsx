30d267f5572f4db594bf7c2d5e11ff9dcc730def
yexiyue <yexiyue666@qq.com>
2024-07-16 23:52:29 +0800

    code: url

--- web/src/routers/index.tsx
+++ web/src/routers/index.tsx
@@ -5,31 +5,36 @@ import { Http } from "../pages/Http";
 import { Mqtt } from "../pages/Mqtt";
 
 //@ts-ignore
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        // {
+        //   path: "/",
+        //   element: <Home />,
+        // },
+        {
+          path: "/ws",
+          element: <Ws />,
+        },
+        {
+          path: "/http",
+          index: true,
+          element: <Http />,
+        },
+        {
+          path: "/mqtt",
+          element: <Mqtt />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      // {
-      //   path: "/",
-      //   element: <Home />,
-      // },
-      {
-        path: "/ws",
-        element: <Ws />,
-      },
-      {
-        path: "/http",
-        index: true,
-        element: <Http />,
-      },
-      {
-        path: "/mqtt",
-        element: <Mqtt />,
-      },
-    ],
-  },
-]);
+    basename: "/rust-embedded-study",
+  }
+);
 
 export const Routers = () => {
   return <RouterProvider router={router}></RouterProvider>;
